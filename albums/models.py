9697a64add07e2064dfc1f2a114a7f84930609a3
daniela <d.ortizkosobucki@gmail.com>
2022-06-30 11:54:53 -0400

    apps built, create super user, 127 working

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# class Album(models.Model):
+#     title = models.CharField(max_length=300)
+#     artist = models.ManyToManyFieldField("Artist", on_delete=models.CASCADE, related_name="albums", null=True, blank=True)
+#     created_at = models.CharField(max_length=300)
+
+# class Artist(models.Model):
+
